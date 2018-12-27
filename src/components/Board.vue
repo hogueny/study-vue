dd7dd432bf4a8e256fef19504821ed32c24e9888
김태형 <taehyeong224@gmail.com>
2018-12-28 00:27:33 +0900

    add board form

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,11 +1,52 @@
 <template>
-  <div>
-      안녕 
+  <div class="wrapper">
+    <b-card header-tag="header" footer-tag="footer">
+      <div slot="header">
+        <i class="fa fa-align-justify"></i>
+        <strong>{{" " + getBoardName}}</strong>
+      </div>
+      <b-list-group>
+        <b-list-group-item href="#" active class="flex-column align-items-start">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">List group item heading</h5>
+            <small>3 days ago</small>
+          </div>
+          <p class="mb-1">
+            Donec id elit non mi porta gravida at eget metus. Maecenas
+            sed diam eget risus varius blandit.
+          </p>
+          <small>Donec id elit non mi porta.</small>
+        </b-list-group-item>
+        <b-list-group-item href="#" class="flex-column align-items-start">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">List group item heading</h5>
+            <small class="text-muted">3 days ago</small>
+          </div>
+          <p class="mb-1">
+            Donec id elit non mi porta gravida at eget metus. Maecenas
+            sed diam eget risus varius blandit.
+          </p>
+          <small class="text-muted">Donec id elit non mi porta.</small>
+        </b-list-group-item>
+      </b-list-group>
+    </b-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'board'
-}
+  name: "board",
+  computed: {
+    getBoardName: function() {
+      const board = this.$route.query.name;
+      if (board === "free") {
+        return "자유 게시판";
+      } else if (board === "notice") {
+        return "공지사항";
+      } else {
+        return "이름 없음";
+      }
+    }
+  }
+};
 </script>
